39c9667fd483b0fac32d705dfd131d36d1b41fcd
abedaiki <abe12261994@gmail.com>
2025-01-18 03:18:54 +0000

    books,usersの編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get 'books/show'
   # get 'books/edit'
    # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-    resources :books, only: [:new, :index, :show, :edit, :create, :destroy]
+    resources :books, only: [:new, :index, :show, :edit, :create, :destroy, :update]
   devise_for :users
  root to: "homes#top"
  get'/homes/about' => 'homes#about' ,as: 'about'
